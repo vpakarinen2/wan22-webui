c5919c3cc62238bad8203b99c26dd8db9777728b
Ville Pakarinen <151925563+vpakarinen2@users.noreply.github.com>
2025-09-13 20:31:21 +0300

    Updated app main file

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 from app.ui.i2v_tab import build_i2v_tab
 from app.ui.s2v_tab import build_s2v_tab
-from app.ui.fun_tab import build_fun_tab
+#from app.ui.fun_tab import build_fun_tab
 
 
 def create_app() -> gr.Blocks:
@@ -11,12 +11,11 @@ def create_app() -> gr.Blocks:
         gr.HTML(
             """
         <h1 style="text-align:center; margin: 12px 0; font-size: 32px; font-weight: 700;">
-            Wan2.2 WebUI (I2V + S2V + Control)
+            Wan2.2 WebUI (I2V + S2V)
         </h1>
             """
         )
         with gr.Tabs():
             build_i2v_tab()
             build_s2v_tab()
-            build_fun_tab()
     return wangd
